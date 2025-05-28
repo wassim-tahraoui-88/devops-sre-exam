4e2a5ebc0c1402e480df957fc169606c884383cd
Wassim Tahraoui <wassim.tahraoui88@gmail.com>
2025-05-28 09:43:09 +0100

    - Added prometheus

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
+from prometheus_client import Counter, generate_latest, Summary
 
 app = Flask(__name__)
 
@@ -12,6 +13,9 @@ products = {
 
 sales_history = []
 
+viewByProduct = Counter("view_by_product", "Number of views by product",["product"])
+salesDuration = Summary("sales_duration", "sales response time")
+
 @app.route('/')
 def index():
     return render_template('index.html', products=products)
@@ -24,6 +28,8 @@ def calculate():
     if product_info is None:
         return "Invalid product", 400
 
+    viewByProduct.labels(product=product_info['description']).inc()
+
     quantity = int(request.form['quantity'])
     total = quantity * product_info['price']
     sales_history.append({
@@ -34,12 +40,17 @@ def calculate():
     return render_template('result.html', product=product_info['description'], quantity=quantity, total=total)
 
 @app.route('/sales')
+@salesDuration.time()
 def sales():
     return render_template('sales.html', sales=sales_history)
 
 @app.route('/back')
 def back():
     return redirect(url_for('index'))
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(),mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=True)
